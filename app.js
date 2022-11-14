553ef7a7c52481477107d6bbce0429f44e9bb4d3
Pedro Moreira <pedrohenryque.rm@gmail.com>
2022-11-14 23:02:16 +0000

    adding basic functions to show numbers in the display

--- app.js
+++ app.js
@@ -1,3 +1,33 @@
+const result = document.querySelector(".result");
+let current = [];
+let previous = [];
+
+//pressing numbers
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("number")) {
+    current.push(e.target.innerText);
+    if (current.length > 15) {
+      alert("this number is too big!");
+      return;
+    }
+    result.innerText = current.join("");
+  }
+});
+//pressing operators
+//pressing dot
+//pressing clear
+const clearBtn = document.querySelector("#clear__btn");
+
+//pressing delete
+
+const deleteBtn = document.querySelector("#dlt__btn");
+deleteBtn.addEventListener("click", () => {
+  if (current.length >= 1) {
+    current.pop();
+    result.innerText = current.join("");
+  }
+});
+
 const multiply = (a, b) => {
   return a * b;
 };
