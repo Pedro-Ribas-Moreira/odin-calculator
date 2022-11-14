51c4ee11d68b0c2a93f7ebe26e8dabfe9ae09497
Pedro Moreira <pedrohenryque.rm@gmail.com>
2022-11-14 22:45:48 +0000

    create basic functions for calculator

---
+++ app.js
@@ -0,0 +1,13 @@
+const multiply = (a, b) => {
+  return a * b;
+};
+const sum = (a, b) => {
+  return a + b;
+};
+const minus = (a, b) => {
+  return a - b;
+};
+
+const divide = (a, b) => {
+  return a / b;
+};
